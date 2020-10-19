33d549cf29208ee7cf6494558d71dd485011962d
vrustik <vrustik4335@gmail.com>
2020-10-19 20:24:04 +0530

    Updates

---
+++ precache-manifest.3114b42d81a0ff99cee168818348b36d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a2687576edaebf0dea24615910cbd47",
+    "url": "/react-notes-example/index.html"
+  },
+  {
+    "revision": "057991a2158360f0abdc",
+    "url": "/react-notes-example/static/css/main.0fa840f8.chunk.css"
+  },
+  {
+    "revision": "fdf0b52e0dc892c3b234",
+    "url": "/react-notes-example/static/js/2.a0df8be1.chunk.js"
+  },
+  {
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "/react-notes-example/static/js/2.a0df8be1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "057991a2158360f0abdc",
+    "url": "/react-notes-example/static/js/main.8d3d6b85.chunk.js"
+  },
+  {
+    "revision": "ebca62a160ca5f63cac0",
+    "url": "/react-notes-example/static/js/runtime-main.f3ac74b9.js"
+  },
+  {
+    "revision": "cddaa833416848bb7165e516f13b8bf1",
+    "url": "/react-notes-example/static/media/Notes.cddaa833.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-notes-example/precache-manifest.3114b42d81a0ff99cee168818348b36d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-notes-example/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
